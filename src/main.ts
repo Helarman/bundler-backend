918e83f1451144ded12fef615b9e84c2280f8636
Helarman <helarman@bk.ru>
2025-07-04 21:06:11 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://bundler-frontend-tlkm.onrender.com/'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://bundler-frontend-tlkm.onrender.com'],
     credentials: true,
     allowedHeaders: [
       'Authorization',
