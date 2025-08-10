1496253b56195fd35761b33dbd57e9e046a8ceef
Helarman <helarman@bk.ru>
2025-08-10 21:37:46 +0300

    Delete dist directory

--- dist/src/modules/solana-dca/dtos/update-dca-account.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateDcaAccountDto = void 0;
-const swagger_1 = require("@nestjs/swagger");
-const dca_order_account_entity_1 = require("../entities/dca-order-account.entity");
-class UpdateDcaAccountDto extends (0, swagger_1.PartialType)((0, swagger_1.OmitType)(dca_order_account_entity_1.DcaAccountEntity, ["accountId", "createdAt", "updatedAt"])) {
-}
-exports.UpdateDcaAccountDto = UpdateDcaAccountDto;
